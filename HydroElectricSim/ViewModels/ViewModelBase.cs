f6f0b4ff851bff79f184826581e1fdd326544dba
tungsten <koszutowski.bartek@gmail.com>
2026-08-21 18:33:13 +0200

    Init

---
+++ HydroElectricSim/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace HydroElectricSim.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ HydroElectricSim/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia;
+using Avalonia.Controls;
+
+namespace HydroElectricSim.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
