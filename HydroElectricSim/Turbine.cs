799ef187b2254ce1c705725d26182c465a4fc8aa
tungsten <koszutowski.bartek@gmail.com>
2026-08-22 11:25:01 +0200

    Turbine!

---
+++ HydroElectricSim/Turbine.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+
+namespace HydroElectricSim;
+
+internal static class Turbine
+{
+    private static List<double> speedHistory = new();
+    internal static double GetTurbineSpeed(double speed, double wicketPosition)
+    {
+        if(wicketPosition<0.1)return speed-(speed/200);
+        //https://www.desmos.com/calculator/4v6lmt0y3w
+        return speed+(30*wicketPosition/(speed+2));
+    }
+    internal static double GetTurbineOutput(double wicketPosition)
+    {
+        return Math.Max(wicketPosition*0.2-(10*0.15),0);
+    }
+    internal static void UpdateSpeedHistory(double speed)
+    {
+        speedHistory.Add(speed);
+        if (speedHistory.Count > 10)
+        {
+            speedHistory.RemoveAt(0);
+        }
+    }
+
+    internal static double GetSpeedStdDev()
+    {
+        if (speedHistory.Count == 0) return 0;
+        double mean = 0;
+        foreach (double speed in speedHistory)
+            mean += speed;
+        mean/= speedHistory.Count;
+        double variance = 0;
+        foreach (double speed in speedHistory)
+            variance += (speed - mean) * (speed - mean);
+        variance/=speedHistory.Count;
+        return Math.Sqrt(variance);
+    }
+}
--- HydroElectricSim/Views/MainView.axaml.cs
+++ HydroElectricSim/Views/MainView.axaml.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
@@ -13,12 +14,19 @@ namespace HydroElectricSim.Views;
 public partial class MainView : UserControl
 {
     DispatcherTimer _timer;
+    DispatcherTimer _demandTimer;
     double Flowrate=0.2;
     double StatorTemp=30;
+    double WicketPosition=0;
+    double Demand=0;
+    double Production=0;
+    double rpm=0;
+    double oilTempValue=15;
     bool TurbineFilling = false;
     bool Miv=false;
     bool ElecOilPump=false;
     bool MainOilPump=false;
+    bool syncState = false;
     
     public MainView()
     {
@@ -27,24 +35,46 @@ public partial class MainView : UserControl
         _timer.Interval = TimeSpan.FromMilliseconds(200);
         _timer.Tick+= Timer_Tick;
         _timer.Start();
+        _demandTimer=new();
+        _demandTimer.Interval=TimeSpan.FromSeconds(45);
+        _demandTimer.Tick+= DemandTimerOnTick;
+        _demandTimer.Start();
+        DemandTimerOnTick(null, null);
     }
+
+    private void DemandTimerOnTick(object? sender, EventArgs e)
+    {
+        Demand=Random.Shared.NextDouble() * (15 - 1) + 1;
+        demandLabel.Content=$"Demand: {Demand:F2} MW";
+    }
+
     private void Timer_Tick(object? sender, EventArgs e)
     {
         GeneratorTick();
+        TurbineTick();
+        HydraulicTick();
         infoUpdate();
     }
 
+    private void HydraulicTick()
+    {
+        oilTempValue+= Production / 180;
+        if (oilCoolPump.IsChecked == true && isCoolingActive())
+            oilTempValue -= Flowrate / 40;
+        oilTemp.Content = $"Oil Temperature: {oilTempValue:F0}C";
+        oilTempBar.Value = oilTempValue;
+    }
     private void GeneratorTick()
     {
         if(genPreheat.IsChecked == true&& StatorTemp<120)
         {
-            StatorTemp += 0.2;
+            StatorTemp += 0.4*Random.Shared.NextDouble();
             genTemp.Content = $"Stator Temperature: {StatorTemp:F1}C";
             genTempBar.Value = StatorTemp;
         }
-        else if(genCoolant.IsChecked == true && StatorTemp>15)
+        else if(genCoolant.IsChecked == true && StatorTemp>15&&isCoolingActive())
         {
-            StatorTemp -= 0.2;
+            StatorTemp -= 0.4*Random.Shared.NextDouble();
             genTemp.Content = $"Stator Temperature: {StatorTemp:F1}C";
             genTempBar.Value = StatorTemp;
         }
@@ -54,11 +84,11 @@ public partial class MainView : UserControl
         Flowrate = Math.Round(e.NewValue, 2);
         flowrateLabel.Content = $"Flowrate: {Flowrate} m³/s";
     }
-    private void GenPreheat_Checked(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    private void GenPreheat_Checked(object? sender, RoutedEventArgs e)
     {
         genCoolant.IsChecked = false;
     }
-    private void GenCoolant_Checked(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    private void GenCoolant_Checked(object? sender, RoutedEventArgs e)
     {
         genPreheat.IsChecked = false;
     }
@@ -81,6 +111,37 @@ public partial class MainView : UserControl
         MIVopen.IsEnabled=false;
         Miv=true;
     }
+
+    private void TurbineTick()
+    {
+        if (!syncState)
+        {
+            if (Miv && !brake.IsChecked == true)
+            {
+                rpm = Turbine.GetTurbineSpeed(rpm, WicketPosition);
+                RpmLabel.Content = $"Turbine Speed: {rpm:F1} rpm";
+                RpmBar.Value = rpm;
+            }
+            else if (brake.IsChecked == true)
+            {
+                rpm -= 0.5;
+                if (rpm < 0) rpm = 0;
+                RpmLabel.Content = $"Turbine Speed: {rpm:F1} rpm";
+                RpmBar.Value = rpm;
+            }
+
+            Turbine.UpdateSpeedHistory(rpm);
+            checkSyncStatus();
+        }
+        else
+        {
+            Production=Turbine.GetTurbineOutput(WicketPosition);
+            prodLabel.Content = $"Power Output: {Production:F2} MW";
+        }
+    }
+    
+
+
     private async Task<bool> isOilPumpRunning()
     {
         if(elecPump.IsChecked==true || MainOilPump)
@@ -93,6 +154,72 @@ public partial class MainView : UserControl
                 .GetMessageBoxStandard("Error!", "Unable to open valve! Is the oil pump running?", ButtonEnum.Ok);
             await box.ShowAsPopupAsync(this);
             return false;
+        }
+    }
+    private bool isCoolingActive()
+    {
+        return coolantValve.IsChecked==true&&loop1.IsChecked==true&&loop2.IsChecked==true;
+    }
+    private void checkSyncStatus()
+    {
+        sync.IsEnabled=false;
+        if (rpm < 245)
+        {
+            syncStatus.Content="Cant sync: too slow!";
+            return;
+        }
+        if(rpm>255)
+        {
+            syncStatus.Content="Cant sync: too fast!";
+            return;
+        }
+        if(Turbine.GetSpeedStdDev()>0.5)
+        {
+            syncStatus.Content="Cant sync: unstable speed!"+$" (StdDev: {Turbine.GetSpeedStdDev():F2}>0.5)";
+            return;
+        }
+        syncStatus.Content="Ready to sync!";
+        sync.IsEnabled=true;
+        
+        
+    }
+
+    private void wicketOpenFine_Click(object? sender, RoutedEventArgs e)
+    {
+        if(WicketPosition+0.1>=100)
+            WicketPosition=100;
+        else
+            WicketPosition+=0.1;
+        UpdateWicketPosition();
+    }
+
+    private void wicketCloseFine_Click(object? sender, RoutedEventArgs e)
+    {
+        if (WicketPosition - 0.1 <= 0)
+            WicketPosition = 0;
+        else
+            WicketPosition -= 0.1;
+        UpdateWicketPosition();
+    }
+    private void UpdateWicketPosition()
+    {
+        wicketPositionLabel.Content = $"Wicket gates: {WicketPosition:F1}%";
+        wicketGates.Value = WicketPosition;
+    }
+
+    private void WicketGates_OnValueChanged(object? sender, RangeBaseValueChangedEventArgs e)
+    {
+        WicketPosition = e.NewValue;
+        UpdateWicketPosition();
+    }
+
+    private void Sync_OnIsCheckedChanged(object? sender, RoutedEventArgs e)
+    {
+        syncState = sync.IsChecked==true;
+        if (syncState)
+        {
+            rpm = 250;
+            RpmLabel.Content = $"Turbine Speed: {rpm:F1} rpm";
         }
     }
 }
